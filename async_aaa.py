1d69f1b2be8f711e5c41995fe264ffc5842933ac
SystemX <morrolumix@mail.ru>
2026-08-06 14:21:45 +0300

    правка

--- async_aaa.py
+++ async_aaa.py
@@ -5,7 +5,7 @@ async def main():
     async with async_playwright() as p:
         browser = await p.chromium.launch()
         page = await browser.new_page()
-        await page.goto("https://lavk.yandex.ru/")
+        await page.goto("https://www.litres.ru/")
         await page.screenshot(path="FirstAsyncTest.png")
         await browser.close()
 
